00492ea882363c95c40bf48bccb8770957243787
voxid02 <129301401+voxid02@users.noreply.github.com>
2023-07-26 01:23:09 +0300

    Add files via upload

---
+++ Hw-3/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import Mainpage from './pages/MainPage';
+import UsersPage from './pages/userPage/UsersPage';
+
+function App() {
+  return (
+    <div className='app'>
+      {/* <Mainpage /> */}
+      <UsersPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Hw-3/my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './redux/reducers/reducer';
+import App from './App';
+
+const store = createStore(rootReducer)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
